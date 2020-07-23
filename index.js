e4dd891be5799baabffd5abc09d1b9c48df0a2a5
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-07-23 17:19:31 -0500

    Support Lambda Layers

--- index.js
+++ index.js
@@ -1,38 +1,90 @@
 var AWS = require("aws-sdk");
 var lambda = new AWS.Lambda();
 
-const zipMap = {
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////     Configuration    //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+// A map of zip files to their associated Lambda Functions
+// Used to update Lambda Function Code
+const fnCodeMap = {
   "corpsmap-instrumentation-api.zip": "corpsmap-instrumentation-api",
   "corpsmap-cumulus-api.zip": "corpsmap-cumulus-api",
 };
 
-// Return lambda function name for a given zipfile name
-const functionName = (zipName) => zipMap[zipName];
+// A map of zip files to their associated Lambda Layers
+// Used to Create Lambda Layer Versions
+const layerMap = {
+  "corpsmap-cumulus-geolambda-base.zip": {
+    LayerName: "corpsmap-cumulus-geolambda-base",
+    CompatibleRuntimes: ["python3.7"],
+  },
+  "corpsmap-cumulus-geolambda-python.zip": {
+    LayerName: "corpsmap-cumulus-geolambda-python",
+    CompatibleRuntimes: ["python3.7"],
+  },
+};
+//////////////////////////////////////////////////////////////////////////////
+////////////////////////// End of Configuration //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+// ZIP File Uploaded is for a Lambda Layer
+const publishLayer = ({ S3Key, S3Bucket, LayerName, CompatibleRuntimes }) => {
+  const params = {
+    Content: {
+      S3Bucket: S3Bucket,
+      S3Key: S3Key,
+    },
+    CompatibleRuntimes: CompatibleRuntimes,
+    LayerName: LayerName,
+  };
+
+  lambda.publishLayerVersion(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+    }
+  });
+};
+
+// ZIP File Uploaded is Lambda Function Code
+const updateFunctionCode = ({ FunctionName, S3Key, S3Bucket }) => {
+  const params = {
+    FunctionName: FunctionName,
+    S3Key: S3Key,
+    S3Bucket: S3Bucket,
+  };
+
+  lambda.updateFunctionCode(params, (err, data) => {
+    if (err) {
+      console.log(
+        `FAIL; Update Code for Lambda Function: ${params.FunctionName}`
+      );
+      console.log(err);
+    } else {
+      console.log(
+        `SUCCESS; Update Code for Lambda Function: ${params.FunctionName}`
+      );
+    }
+  });
+};
 
 exports.handler = function (event, context) {
   const key = event.Records[0].s3.object.key;
   const bucket = event.Records[0].s3.bucket.name;
   console.log(`Create Event Detected in Bucket: ${bucket}; Key ${key}`);
 
-  if (bucket == "corpsmap-lambda-zips" && zipMap.hasOwnProperty(key)) {
-    const fn = functionName(key);
-    console.log(`Update Code for Lambda Function: ${fn}`);
-
-    const params = {
-      FunctionName: fn,
-      S3Key: key,
-      S3Bucket: bucket,
-    };
-
-    // Update function code
-    lambda.updateFunctionCode(params, function (err, data) {
-      if (err) {
-        console.log(`FAIL; Update Code for Lambda Function: ${fn}`);
-        console.log(err);
-      } else {
-        console.log(`SUCCESS; Update Code for Lambda Function: ${fn}`);
-      }
-    });
+  if (fnCodeMap.hasOwnProperty(key)) {
+    // .zip is Lambda Function Code
+    const fn = fnCodeMap[key];
+    console.log(`Update Lambda Function Code: ${fn}`);
+    updateFunctionCode({ FunctionName: fn, S3Key: key, S3Bucket: bucket });
+  } else if (layerMap.hasOwnProperty(key)) {
+    // .zip is Lambda Function Layer
+    const cfg = layerMap[key];
+    console.log(`Update Lambda Layer: ${cfg.LayerName}`);
+    publishLayer({ ...cfg, S3Key: key, S3Bucket: bucket });
   } else {
     context.succeed(`skipping zip ${key} in bucket ${bucket}`);
   }
